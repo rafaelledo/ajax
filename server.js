f7db34040d3127dee8c31d3559b0c739807075b6
Rafael Ledo <rafaeloliveiraledo@gmail.com>
2022-11-19 11:41:51 -0300

    update server.js and xmlHttpRequest2.html

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok'))
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './upload')
+  },
+  filename: function (req, file, callback) {
+    callback(null, `${Date.now()}_${file.originalname}`)
+  }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      return res.end('Ocorreu um erro.')
+    }
+    res.end('Concluído com sucesso.')
+  })
+})
 
 app.listen(3000, () => console.log('Executando...'))
