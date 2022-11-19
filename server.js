1b196d4eeafdc3d1cd4ac0f44604fd5df451e954
Rafael Ledo <rafaeloliveiraledo@gmail.com>
2022-11-19 18:59:27 -0300

    add changes on various files

--- server.js
+++ server.js
@@ -28,4 +28,11 @@ app.post('/upload', (req, res) => {
   })
 })
 
+app.post('/formulario', (req, res) => {
+  res.send({
+    ...req.body,
+    id: 1
+  })
+})
+
 app.listen(3000, () => console.log('Executando...'))
